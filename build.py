f607ca4a8ae3d938dffcd6c764c767f76c6a9d66
Gustavo Pinho Kretzer de Souza <gustavo@bry.com.br>
2018-03-01 12:13:10 -0300

    Update build.py

--- build.py
+++ build.py
@@ -5,6 +5,9 @@ def run(command):
     if ret != 0:
         raise Exception("Error in: %s" % command)
 
+run("conan create dependencies user/testing -s arch=x86")
+run("conan create dependencies user/testing -s arch=x86_64")
+
 run("conan create driver user/testing -s arch=x86")
 run("conan create driver user/testing -s arch=x86_64")
 
